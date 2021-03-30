626b83ef02e334b9962616b34284a97ed4aae17a
Guna <gunagang24@gmail.com>
2021-03-30 23:14:21 +0530

    did lots of stuff, like create post options, update post etc

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,12 @@
 
 from django.urls import path, include
 from .import views 
+from blog.views import postListView, postDetailView, postCreateView
 
 urlpatterns = [
-    path('' , views.home, name='Blog_Home'),
+    path('' , postListView.as_view(), name='Blog_Home'),
+    path('post/<int:pk>' , postDetailView.as_view(), name='postDetail'),
+    path('post/create/', postCreateView.as_view(), name='postCreate'),
     path('about/', views.about, name='Blog_About'),
     path('contact/', views.contact, name='Contact'),
 ]	
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import post, Threads
-# Create your views here.
+from django.views.generic import ListView, DetailView, CreateView
 
 def home(request):
 	context={
 	'dicpost': post.objects.all(),
-	'dicThreads': Threads.objects.all()
 	}
 	return render(request, 'blog/bloghome.html', context)
+
+class postListView(ListView):
+	model = post
+	template_name = 'blog/bloghome.html'
+	context_object_name = 'dicpost'
+	ordering = ['-datePosted']
+
+class postDetailView(DetailView):
+	model = post
+
+class postCreateView(CreateView):
+	model = post
+	fields = ['title', 'content']
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
 
 def about(request):
 	return render(request, 'blog/blogabout.html', {'title':'BlogAbout'})
