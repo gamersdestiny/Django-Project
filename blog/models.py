3b5c75a3058fe046f8349ae934ff3efd148117ff
Guna <gunagang24@gmail.com>
2021-04-10 12:22:36 +0530

    models

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from PIL import Image
 from django.urls import reverse
 
 class post(models.Model):
