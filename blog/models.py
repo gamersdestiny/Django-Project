c0c64a2626e5eb791129a4424829a3a469d3c419
Guna <gunagang24@gmail.com>
2021-04-10 13:01:14 +0530

    upload working

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-from PIL import Image
 from django.urls import reverse
 
 class post(models.Model):
 	title = models.CharField(max_length=100)
 	content = models.TextField()
-	shortDescription = models.TextField()
+	shortDescription = models.TextField(blank=True)
 	datePosted = models.DateTimeField(default= timezone.now)
-	image = models.ImageField(default="filedefault.jpg", upload_to='post_pictures')
+	image = models.FileField(default="filedefault.jpg", null=True, blank=True, upload_to='post_pictures')
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
 	
 	def __str__(self):
