5d3c6f15268b089cdce6b5c8eb8c7bd25b3830b5
Guna <71771581+gamersdestiny@users.noreply.github.com>
2021-08-13 18:37:52 +0530

    removed comments

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ class post(models.Model):
 	title = models.CharField(max_length=100)
 	content = models.TextField()
 	shortDescription = models.TextField(blank=True)
-	datePosted = models.DateTimeField(default= timezone.now)
+	datePosted = models.DateTimeField(default=timezone.now)
 	image = models.FileField(default="filedefault.jpg", null=True, blank=True, upload_to='post_pictures')
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
 	likes = models.ManyToManyField(User, related_name="liked")
@@ -19,11 +19,3 @@ class post(models.Model):
 	def get_absolute_url(self):
 		return reverse( 'postDetail', kwargs={'pk':self.pk} )
 
-# class Threads(models.Model):
-# 	threadName = models.CharField(max_length=30)
-# 	threadDescription = models.TextField()
-# 	datePosted = models.DateTimeField(default=timezone.now)
-# 	author = models.ForeignKey(User, on_delete=models.CASCADE)
-
-# 	def __str__(self):
-# 		return self.threadName
